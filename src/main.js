1a8b18a98aa95986b8b0983ca2a4d0810dc69a40
James Trickey <james.trickeyd@gmail.com>
2017-06-17 23:59:33 +0930

    first

---
+++ src/main.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer();
+
+http.createServer(function(req, res){
+    switch(req.headers.host){
+        case "kathleenprior.co.uk":
+        case "www.kathleenprior.co.uk":
+            proxy.web(req, res, { target: 'http://localhost:8083' });
+            break;
+        case "swannell-portraits.com":
+        case "www.swannell-portraits.com":
+            proxy.web(req, res, { target: 'http://localhost:8084' });
+            break;
+        case "jamestrickey.com":
+        case "www.jamestrickey.com":
+            proxy.web(req, res, { target: 'http://localhost:8085' });
+            break;
+    }
+}).listen(8082);
